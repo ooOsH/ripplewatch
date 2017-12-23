9569aad77658c89582341015dd1504a7bbef6fa0
ginger3186 <lee@levservices.net>
2017-12-23 16:35:19 +0000

    Graph Gen
    Issue with graph not ouputing

--- assets/src/js/scripts.js
+++ assets/src/js/scripts.js
@@ -46,104 +46,119 @@ $(function() {
     // Initial Load
     getData();
 
-    // Add data to graph
-    var ctx = $('#rippleChart');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        responsive: true,
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: 'Price of XRP',
-                // x time / y price
-                data: [{
-                        x: "13:56",
-                        y: "0.41"
-                    }, {
-                        x: "13:57",
-                        y: "0.45"
-                    }, {
-                        x: "13:58",
-                        y: "0.44"
-                    }],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
-
-
     // Refresh Price every 15 sec
     setInterval(function() {
         getData();
     }, 15000);
 
 
+
+
     // get all data for graph
-        var all_coin_data;
-        var last_hour;
-        var graph_data = [];
+    var all_coin_data;
+    var last_hour;
+    var graph_data = "";
+    var labels_data = ""; 
 
-        function generateCoinData() {
-            // 1440 request for a 24 hour graph with 1 min intervals
-            $.getJSON('https://min-api.cryptocompare.com/data/histominute?fsym=XRP&tsym=USD&limit=60&aggregate=3&e=CCCAGG', function(json){
-                all_coin_data = json;
-                var unix_time;
-                var price_min;
+    function generateGraphData() {
+        // 1440 request for a 24 hour graph with 1 min intervals
+        $.getJSON('https://min-api.cryptocompare.com/data/histominute?fsym=XRP&tsym=USD&limit=20&aggregate=3&e=CCCAGG', function(json){
+            all_coin_data = json;
+            var unix_time;
+            var price_min;
 
-                $.each( all_coin_data, function(k,v) {
-                    // console.log("key = " + k);
-                    // console.log("value = " + v);
+            $.each( all_coin_data, function(k,v) {
+                // console.log("key = " + k);
+                // console.log("value = " + v);
 
-                    if (k == "Data") {
-                        last_hour = v;
-                        console.log(last_hour); 
-                        $.each(last_hour, function(index) {
-                            unix_time = last_hour[index].time;
-                            price = last_hour[index].close;
-                            // console.log("unix time = " + unix_time);
-                            // console.log("price min = " + price);
-                            var timestampInMilliSeconds = unix_time*1000;
-                            var formated_time = new Date(timestampInMilliSeconds);
-                            console.log(formated_time);
-                        });
-                    } else {
+                if (k == "Data") {
+                    last_hour = v;
+                    //console.log(last_hour); 
+                    $.each(last_hour, function(index) {
+                        unix_time = last_hour[index].time;
+                        price = last_hour[index].close;
+                        // console.log("unix time = " + unix_time);
+                        // console.log("price min = " + price);
+                        var timestampInMilliSeconds = unix_time*1000;
+                        var formated_time_date = new Date(timestampInMilliSeconds);
+                        var hrs = formated_time_date.getHours();
+                        var mins = formated_time_date.getMinutes();
+                        if (mins < 10) {
+                            mins = "0" + mins;
+                        } else {
 
-                    }
+                        }
+                        var seconds = formated_time_date.getSeconds();
+                         if (seconds < 10) {
+                            seconds = "0" + seconds;
+                        } else {
 
-                });
-                console.log("graph data = " + graph_data[0]);
+                        }
+                        var formated_time = hrs + ":" + mins;
+
+                        graph_data = graph_data + '{x: "' + formated_time + '",y: ' + price + '},';
+                        labels_data = labels_data + '"' + formated_time + '",';
+
+                    });
+                } else {
+
+                }
+
             });
 
-           return last_hour;
-        }
+            graph_data = graph_data.substring(0,graph_data.length - 1);
+            labels_data = labels_data.substring(0,labels_data.length - 1);
 
-        // Initial Load
-        generateCoinData();
+            console.log("graph data - " + graph_data);
+            console.log("labels data - " + labels_data);
+
+            // Add data to graph
+            var ctx = $('#rippleChart');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                responsive: true,
+                data: {
+                    labels: [labels_data],
+                    datasets: [{
+                        label: 'Price of XRP',
+                        // x time / y price
+                        data: [graph_data],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+            
+        });
+    }
+
+    // Initial Load
+    generateGraphData();
+
 
 
 
